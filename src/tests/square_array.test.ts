c4f29387a8862206c4fb04cd1a30534f1a13cce8
Ana Barbosa (Escolar) <nokisegunda@gmail.com>
2026-04-06 20:32:56 +0000

    test: adiciona novo teste e corrige teste antigo

--- src/tests/square_array.test.ts
+++ src/tests/square_array.test.ts
@@ -5,5 +5,5 @@ test('test square array simple for', () => {
 })
 
 test('test square array foreach', () => {
-    expect(square_array_for([3,5,7,3,8,9,1])).toEqual([9,25,49,9,64,81,1])
+    expect(square_array_foreach([3,5,7,3,8,9,1])).toEqual([9,25,49,9,64,81,1])
 })
